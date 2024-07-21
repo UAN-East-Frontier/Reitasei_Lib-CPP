484b67fdb9add5a6ad3259181839fbea45e6bef6
GensokyoPerson <samplay4ik@gmail.com>
2024-07-21 18:56:33 +0300

    sinWave&CircleVector

--- Matan.cpp
+++ Matan.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <chrono>
+#include "Matan.h"
 
 int getRandomNumber(int min, int max)
 {
@@ -13,3 +12,14 @@ int getRandomNumber(int min, int max)
     std::srand(std::chrono::high_resolution_clock::now().time_since_epoch().count());
     return min + rand() % (max - min + 1); //+1 need because include max number in range
 }
+
+float sinWave(float x, float a, float b, float c) {
+    return a * sin(b * x + c);
+}
+
+sf::Vector2f circleMoving(sf::Vector2f center, float velocity, float radius, float time) { //Формула №6
+    float x = center.x + radius * cos(velocity * time);
+    float y = center.y + radius * sin(velocity * time);
+    return sf::Vector2f(x, y);
+}
+
--- Matan.h
+++ Matan.h
@@ -1,3 +1,11 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <chrono>
 
 int getRandomNumber(int min, int max);
+
+float sinWave(float x, float a, float b, float c);
+
+sf::Vector2f circleMoving(sf::Vector2f center, float velocity, float radius, float time);
+
